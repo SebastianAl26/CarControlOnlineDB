5656bdec5cfd6e54668a8efd879dd17ad514e7e3
Sebastian <aldoav231224@gmail.com>
2024-12-04 23:46:08 -0600

    [MODIFIED][MAIN] s-01-usuarios.sl script corregido

--- scripts/s-01-usuarios.sql
+++ scripts/s-01-usuarios.sql
@@ -3,28 +3,29 @@
 --@Fecha creación: 25/11/2024
 --@Descripción: Se crean los usuarios admin e invitado y sus roles correspondientes
 
-prompt conectando como sys a la pdb 
+whenever sqlerror exit;
+prompt conectando como sys a la pdb
 connect sys/system1@afbd_s2 as sysdba
 
 prompt creando roles
 drop role if exists rol_admin;
 create role rol_admin;
 grant create session, create table, create sequence, create view,
-  create synonym, create procedure, create trigger to rol_admin;
+  create synonym, create public synonym, create procedure, create trigger to rol_admin;
 
 drop role if exists rol_invitado;
 create role rol_invitado;
-grant create session to rol_invitado;
+grant create session, create synonym to rol_invitado;
 
 prompt se creando usuarios
-drop user if exists af_proy_admin;
+drop user if exists af_proy_admin cascade;
 create user af_proy_admin identified by af quota unlimited on users;
 
-drop user if exists af_proy_invitado;
+drop user if exists af_proy_invitado cascade;
 create user af_proy_invitado identified by af quota unlimited on users;
 
 prompt asignando roles a los usuarios
 grant rol_admin to af_proy_admin;
-grant rol_invitado to af_proy_admin;
+grant rol_invitado to af_proy_invitado;
 
 prompt script finalizado
